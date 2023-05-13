b661aa3b286492d0ee1519823aa0b70377a539b4
david <davidmoerdijk@gmail.com>
2023-05-13 15:54:11 +0200

    bing AI returns correct dict

--- EdgeGPT/main.py
+++ EdgeGPT/main.py
@@ -1,10 +1,27 @@
 import asyncio
 from EdgeGPT import Chatbot, ConversationStyle
+import tracemalloc
 
-import os
+async def main():
+    asyncio.create_task(other_func()) # run this when you find tasks to complete while waiting for API calls etc.
+    bot = await Chatbot.create(cookie_path='EdgeGPT/cookies.json')
+    # user_request = input("Finish the sentence: Show me all countries ")
+    user_request = "that speak french"
+    correct_example = "{
+    'French Speaking Countries': ['Belgium', 'Benin', 'Burkina Faso', 'Burundi', 'Cameroon', 'Canada', 'Central African Republic', 'Chad', 'Comoros', 'Democratic Republic of the Congo', 'Djibouti', 'Equatorial Guinea', 'France', 'Gabon', 'Guinea', 'Haiti', 'Ivory Coast', 'Luxembourg', 'Madagascar', 'Mali', 'Monaco', 'Niger', 'Republic of the Congo', 'Rwanda', 'Senegal', 'Seychelles', 'Switzerland', 'Togo'],
+    'Spanish Speaking Countries': ['Argentina','Bolivia','Chile','Colombia','Costa Rica','Cuba','Dominican Republic','Ecuador','El Salvador','Equatorial Guinea','Guatemala','Honduras','Mexico','Nicaragua','Panama','Paraguay','Peru','Puerto Rico','Spain','Uruguay','Venezuela']}"
+    chatbot_input = "Show me all countries " + user_request + ". Only write the answer in a python dictionary, using the categories (e.g. 'french speaking' )as keys and a list of the countries as values (e.g. ['France', 'Canada', 'Others']). Here is an example of a correct dictionary for french and spanish speaking countries. " + correct_example
+     response = await bot.ask(prompt=chatbot_input, conversation_style=ConversationStyle.creative)
+    for message in response["item"]["messages"]:
+        if message["author"] == "bot":
+            bot_response = message["text"]
+    await bot.close()
 
-# print(os.getcwd())
+    print(bot_response)
 
+async def other_func(): # function that can be run while main waits for API etc.
+    return None
 
-bot = Chatbot(cookie_path='\EdgeGPT\cookies.json')
-print(bot.ask(prompt="Hello there", conversation_style=ConversationStyle.creative))
+if __name__ == "__main__":
+    asyncio.run(main())
+
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Map-Creator
 Uses LLM to create any map a user can think of. 
+
+
+
+# Concepts to add:
+Results might not always be accurate, so all data should be able to be easily edited.   
